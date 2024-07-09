3253465b756f20a828e4fad660445c830c84f4b4
K Sadov <ksadov@gmail.com>
2024-07-09 11:57:41 -0500

    add menu item for theme that does not yet work

--- src/components/LoomMenu.tsx
+++ src/components/LoomMenu.tsx
@@ -1,28 +1,15 @@
 import {
   Menubar,
-  MenubarContent,
-  MenubarItem,
   MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarTrigger,
 } from "../@/components/ui/menubar"
+
+import ThemeSwitch from "./ThemeSwitch"
 
 export default function LoomMenu() {
   return (
     <Menubar>
       <MenubarMenu>
-        <MenubarTrigger>File</MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>
-            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-          </MenubarItem>
-          <MenubarItem>New Window</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Share</MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>Print</MenubarItem>
-        </MenubarContent>
+        <ThemeSwitch />
       </MenubarMenu>
     </Menubar>
   );
---
+++ src/components/ThemeSwitch.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { Moon, Sun } from "lucide-react"
+
+import { MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from "../@/components/ui/menubar"
+
+export default function Themeswitch() {
+  return (
+    <div className="size-full flex justify-end">
+      <MenubarMenu>
+        <MenubarTrigger>
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            Dark
+          </MenubarItem>
+          <MenubarItem>
+            Light
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </div>
+  );
+}
