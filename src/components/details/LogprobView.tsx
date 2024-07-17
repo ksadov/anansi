9a22d41226bfd1c93570d2355e26aac7c3b0c33f
K Sadov <ksadov@gmail.com>
2024-07-17 12:36:29 -0500

    add tooltip with logprobs

--- src/components/details/LogprobView.tsx
+++ src/components/details/LogprobView.tsx
@@ -1,12 +1,28 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 import { LoomNode } from "utils/logic/types";
 
 function LinprobHighlight({ token, lp }: { token: string, lp: number }) {
   const linprob = Math.exp(lp);
   const bgColor = `rgba(${Math.round(255 * (1 - linprob))},${Math.round(255 * linprob)},0, 0.5)`;
+  const linprobStr = linprob < 0.01 ? "<0.01" : linprob > 0.99 ? ">0.99" : linprob.toFixed(2);
   return (
-    <span style={{ backgroundColor: bgColor, borderRadius: 4 }}>
-      {token}
-    </span>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <span style={{ backgroundColor: bgColor, borderRadius: 4 }}>
+          {token}
+        </span>
+      </TooltipTrigger>
+      <TooltipContent>
+        <div>
+          <div>{token}: {linprobStr}</div>
+        </div>
+      </TooltipContent>
+    </Tooltip>
   );
 }
 
@@ -16,8 +32,10 @@ export default function LogprobView({ loomNode }: { loomNode: LoomNode }) {
     return <LinprobHighlight key={i} token={logprob.token} lp={logprob.lp} />
   });
   return (
-    <div className="rounded-md border p-2 overflow-scroll max-h-[65vh]">
-      {highlightedText}
-    </div>
+    <TooltipProvider>
+      <div className="rounded-md border p-2 overflow-scroll max-h-[65vh]">
+        {highlightedText}
+      </div>
+    </TooltipProvider>
   );
 }
